ac9268ac383ad6ab9962c42df7681e02d69ac0ce
Phuong Tay <nptay1999@gmail.com>
2022-10-24 22:31:16 +0700

    Add Eslint and prettier

--- components/forms/Input.tsx
+++ components/forms/Input.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { Formik } from 'formik';
 import * as Yub from 'yup';
-import useWelcome from '../../hooks/useWelcome'
+
+import useWelcome from '../../hooks/useWelcome';
 
 const Input = () => {
   const { inputRef, action } = useWelcome();
@@ -12,40 +13,42 @@ const Input = () => {
         fullName: Yub.string()
           .min(4, 'Must be more than 4 charaters')
           .max(50, 'Must be less than 50 charaters')
-          .required('Required')
+          .required('Required'),
       })}
       onSubmit={(values, actions) => {
         console.log(actions);
         const { setSubmitting, resetForm } = actions;
         console.log(values);
-        resetForm()
+        resetForm();
         setSubmitting(false);
       }}
     >
-      {
-        formik => {
-          return (
-            <form onSubmit={formik.handleSubmit}>
-              <div>
-                <input
-                  type="text"
-                  ref={inputRef}
-                  placeholder="Enter your name"
-                  {...formik.getFieldProps('fullName')}
-                />
-                {formik.touched.fullName && formik.errors.fullName ? (
-                  <div className="text-sm text-red-500">{formik.errors.fullName}</div>
-                ) : null}
-              </div>
-              <div className="flex items-center justify-center">
-                <button type="submit" disabled={!formik.errors} onClick={action}>Take Me In!</button>
-              </div>
-            </form>
-          );
-        }
-      }
+      {(formik) => {
+        return (
+          <form onSubmit={formik.handleSubmit}>
+            <div>
+              <input
+                type="text"
+                ref={inputRef}
+                placeholder="Enter your name"
+                {...formik.getFieldProps('fullName')}
+              />
+              {formik.touched.fullName && formik.errors.fullName ? (
+                <div className="text-sm text-red-500">
+                  {formik.errors.fullName}
+                </div>
+              ) : null}
+            </div>
+            <div className="flex items-center justify-center">
+              <button type="submit" disabled={!formik.errors} onClick={action}>
+                Take Me In!
+              </button>
+            </div>
+          </form>
+        );
+      }}
     </Formik>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
